fd697d49552040b1ed7d0f4abe0fa3ab82bc8725
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-06-04 05:50:29 +0200

    Bug hunting

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // Pi = 4/1 - 4/3 + 4/5 - 4/7...
 
 function calcPI(iterations) {
-  let pi = 0,
-    divisor = 1;
+  let pi = 0, divisor = 1;
   for (i = 0; i <= iterations; i++) {
     pi = pi + 4 / divisor - 4 / (divisor + 2);
     divisor += 4;
   }
-  document.getElementById("output").value = pi.toFixed(10);
+  document.getElementById("output1").value = pi.toFixed(10);
 }
 
 // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
